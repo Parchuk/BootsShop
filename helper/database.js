1c2f404644583bf5090d9fb61ad7f5136b3ef2e3
Parchuk <sergeyparchuk1@gmail.com>
2020-09-06 20:46:51 +0300

    added conect for db

--- helper/database.js
+++ helper/database.js
@@ -2,6 +2,6 @@ const Sequalize = require("sequelize");
 
 const sequalize = new Sequalize("intershop", "root", "passport123", {
     dialect: "mysql",
-    host: "3306",
+    host: "172.17.0.3",
 });
 module.exports = sequalize;
