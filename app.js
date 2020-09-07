9177a29942f581b66a66bc08f82cf0d7e90bdcc8
Parchuk <sergeyparchuk1@gmail.com>
2020-09-07 15:38:52 +0300

    test db

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ sequalize
     .sync()
     .then((result) => {
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-        console.log(result);
+        // console.log(result);
         console.log('dataBase ok!');
     })
     .catch((err) => console.log(err));
--- helper/database.js
+++ helper/database.js
@@ -1,7 +1,7 @@
 const Sequalize = require("sequelize");
 
 const sequalize = new Sequalize("intershop", "root", "passport123", {
-    dialect: "mariadb",
+    dialect: "mysql",
     host: "172.17.0.2",
 });
 module.exports = sequalize;
