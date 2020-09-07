731471dbb508d148cc86b9c254ef58ce073e2f56
Parchuk <sergeyparchuk1@gmail.com>
2020-09-07 16:29:01 +0300

    test db

--- helper/database.js
+++ helper/database.js
@@ -3,6 +3,6 @@ const Sequalize = require("sequelize");
 const sequalize = new Sequalize("intershop", "root", "passport123", {
     dialect: "mysql",
     host: "172.17.0.3",
-    port: 3306
+    port: 8081
 });
 module.exports = sequalize;
