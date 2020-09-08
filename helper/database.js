4c8a90675d4446629c65a862f6f3553068bec0e8
Parchuk <sergeyparchuk1@gmail.com>
2020-09-08 18:15:55 +0300

    added conect for db

--- helper/database.js
+++ helper/database.js
@@ -2,6 +2,6 @@ const Sequalize = require("sequelize");
 
 const sequalize = new Sequalize("intershop", "parchuk", "12345678", {
     dialect: "mysql",
-    host: "172.17.0.2",
+    host: "172.17.0.3",
 });
 module.exports = sequalize;
