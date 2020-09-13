4abaf37186aed116f70cc8beb0012283a6caddb6
Parchuk <sergeyparchuk1@gmail.com>
2020-09-13 11:33:18 +0300

    added credential

--- helper/database.js
+++ helper/database.js
@@ -7,7 +7,8 @@ const Sequalize = require("sequelize");
 
 const sequalize = new Sequalize('intershop', 'parchuk', '12345678', {
     dialect: "mysql",
-    host: '172.17.0.3',
+    host: '172.17.0.2',
 });
 module.exports = sequalize;
+
 
