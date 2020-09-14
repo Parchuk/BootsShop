39afebd6edb17fbf95b302daa65cdf81753aa53e
Parchuk <sergeyparchuk1@gmail.com>
2020-09-14 14:23:09 +0300

    added models

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,39 +1,47 @@
+const Product = require('../models/product');
+
 exports.index = (req, res, next) => {
-    res.render("pages/index")
-}
+    Product.findAll().then(rows => {
+        res.render("pages/index", {
+            products: rows,
+            pageTitle: 'All products',
+            path: '/'
+        });
+    }).catch(err => console.log(err));
+};
 exports.faq = (req, res, next) => {
-    res.render("pages/faq")
-}
+    res.render("pages/faq");
+};
 exports.contact = (req, res, next) => {
-    res.render("pages/contact")
-}
+    res.render("pages/contact");
+};
 exports.delivery = (req, res, next) => {
-    res.render("pages/delivery")
-}
+    res.render("pages/delivery");
+};
 exports.forgetpass = (req, res, next) => {
-    res.render("pages/forgetpass")
-}
+    res.render("pages/forgetpass");
+};
 exports.legal_notice = (req, res, next) => {
-    res.render("pages/legal_notice")
-}
+    res.render("pages/legal_notice");
+};
 exports.login = (req, res, next) => {
-    res.render("pages/login")
-}
+    res.render("pages/login");
+};
 exports.product_details = (req, res, next) => {
-    res.render("pages/product_details")
-}
+    res.render("pages/product_details");
+};
 exports.product_summary = (req, res, next) => {
-    res.render("pages/product_summary")
-}
+    res.render("pages/product_summary");
+};
 exports.products = (req, res, next) => {
-    res.render("pages/products")
-}
+    res.render("pages/products");
+};
 exports.register = (req, res, next) => {
-    res.render("pages/register")
-}
+    res.render("pages/register");
+};
 exports.special_offer = (req, res, next) => {
-    res.render("pages/special_offer")
-}
+    res.render("pages/special_offer");
+};
 exports.tac = (req, res, next) => {
-    res.render("pages/tac")
-}
+    res.render("pages/tac");
+};
--- helper/database.js
+++ helper/database.js
@@ -1,11 +1,11 @@
 const Sequalize = require("sequelize");
-const { password, username } = undefined || require('./config.js');
+const { password, username } = undefined || require('./configl.js');
 const USERNAME = process.env.DB_ACCESS_KEY_ID || username;
 const PASSWORD = process.env.DB_SECRET_ACCESS_KEY || password;
 
-const sequalize = new Sequalize('intershop', USERNAME, PASSWORD, {
+const sequalize = new Sequalize('bootshop', USERNAME, PASSWORD, {
     dialect: "mysql",
-    host: '52.234.178.189',
+    host: '192.168.64.3',
 });
 module.exports = sequalize;
 
