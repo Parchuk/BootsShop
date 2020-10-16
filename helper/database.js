4dc683bbc7c593c1f2d249fad9c7d1583454ed0f
Parchuk <sergeyparchuk1@gmail.com>
2020-10-17 00:28:25 +0300

    new version bootshop

--- helper/database.js
+++ helper/database.js
@@ -1,12 +1,2 @@
-const Sequalize = require("sequelize");
-const { password, username } = undefined || require('./configl.js');
-const USERNAME = process.env.DB_ACCESS_KEY_ID || username;
-const PASSWORD = process.env.DB_SECRET_ACCESS_KEY || password;
-
-const sequalize = new Sequalize('bootshop', USERNAME, PASSWORD, {
-    dialect: "mysql",
-    host: '192.168.64.3',
-});
-module.exports = sequalize;
-
-
+exports.USERNAME = process.env.DB_ACCESS_KEY_ID;
+exports.PASSWORD = process.env.DB_SECRET_ACCESS_KEY;
